0e4db9ba0f1325459dc5e46f022715420c15f627
Jennifer Stoos <jstoos@bitlaw.com>
2018-04-20 21:30:13 +0000

    automatically backed up by learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -1,1 +1,15 @@
-# Create your haunted_houses migration here
+class CreateHauntedHouses < ActiveRecord::Migration[4.2]
+
+  def change
+    create_table :HauntedHouse do |h|
+      h.string :name
+      h.string :location
+      h.string :theme
+      h.float :price
+      h.boolean :family_friendly
+      h.datetime :opening_date
+      h.datetime :closing_date
+      h.text :long_long_description
+  end
+
+end
