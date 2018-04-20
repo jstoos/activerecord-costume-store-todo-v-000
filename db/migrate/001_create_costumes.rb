c72b7e25f35920a2f638bdf6eff41ca8bdf29618
Jennifer Stoos <jstoos@bitlaw.com>
2018-04-20 21:01:27 +0000

    automatically backed up by learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -5,7 +5,7 @@ class CreateCostumes < ActiveRecord::Migration[4.2]
       c.float :price
       c.integer  :size
       c.string  :image_url
-      c.timestamp
+      c.timestamps
     end
   end
 end
