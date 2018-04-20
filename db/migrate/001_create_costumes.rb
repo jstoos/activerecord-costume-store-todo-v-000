2496662d1293508f1a23b1152b7ce9aa7399bc99
Jennifer Stoos <jstoos@bitlaw.com>
2018-04-20 20:43:09 +0000

    automatically backed up by learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -1,7 +1,10 @@
 class CreateCostumes < ActiveRecord::Migration[4.2]
   def change
     create_table :costumes do |c|
-      
+      c.string :name
+      c.float :price
+      c.  :size
+      c.  :image_url
     end
   end
 end
