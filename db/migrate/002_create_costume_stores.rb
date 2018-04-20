31e07f601b8d8912aced6a5530472fdcd61ba542
Jennifer Stoos <jstoos@bitlaw.com>
2018-04-20 21:19:13 +0000

    automatically backed up by learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -1,1 +1,14 @@
-# Create your costume_stores migration here
+class CostumeStore < ActiveRecord::Migration[4.2]
+
+  def change 
+    create_table do |t|
+      t.string :name
+      t.string :location
+      t.string :costume_inventory
+      t.integer :employees_count
+      t.boolean :still_in_business_or_permanently_closed
+      t.datetime :opening_time
+      t.datetime :closing_time
+  end
+
+end
