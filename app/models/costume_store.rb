4a40a0d15c83c9f51898203cce45fba83ba64b71
Jennifer Stoos <jstoos@bitlaw.com>
2018-04-20 20:34:02 +0000

    automatically backed up by learn

--- app/models/costume_store.rb
+++ app/models/costume_store.rb
@@ -1,12 +1,4 @@
  class CostumeStore < ActiveRecord::Base
 
-   def change
-     create_table :costumes do |c|
-       c.name
-       c.price
-       c.size
-       c.image url
-     
-   end
 
  end
